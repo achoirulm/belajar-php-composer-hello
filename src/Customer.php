120115f91b9d58dd16d3614435e70359ac0d360c
Achmad Choirul Mu'minin <choirulmukminin02@gmail.com>
2023-01-27 14:25:11 +0800

    add default value

--- src/Customer.php
+++ src/Customer.php
@@ -8,7 +8,7 @@ class Customer
     {
     }
 
-    public function sayHello(string $var): string
+    public function sayHello(string $var = "Tamu"): string
     {
         return "Hello $var, My Name is $this->var";
     }
